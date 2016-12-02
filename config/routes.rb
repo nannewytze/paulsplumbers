e46d9c33e789e0969afb2730151766c6e57c13fe
Maikymaik12 <maik.keijzer@gmail.com>
2016-12-02 18:09:44 +0100

    create show page and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "problems#index"
+  # root "problems#index"
+  #
+  # get "problems" => "problems#index"
+  # get "problem/:id" => "problem#show"
 
-  get "problems" => "problems#index"
-
+  resources :problems
 end
