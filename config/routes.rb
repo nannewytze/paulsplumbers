e8a0f522495691dcc05c80d7a9fc5db93a6466fd
Nanne Brouwer <info@nannewytze.nl>
2016-12-02 23:45:12 +0100

    fixing conflicting copy within root file

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root "problems#index"
-  #
-  # get "problems" => "problems#index"
-  # get "problem/:id" => "problem#show"
+   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   # root "problems#index"
+   #
+   # get "problems" => "problems#index"
+   # get "problem/:id" => "problem#show"
 
-  resources :problems
-
-
-
-
-
-  resources :plumbers
-
-
-
-
-
-end
+   root "plumbers#show"
+   get "plumbers" => "plumbers#index"
+   resources :problems
+ end
