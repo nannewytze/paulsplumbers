b57961bfb5f88784d31099e2a9e33a1a3a56f17a
Nanne Brouwer <info@nannewytze.nl>
2016-12-02 22:25:59 +0100

    plumbers_index file controler and new rout for plumbers.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   #
   # get "problems" => "problems#index"
   # get "problem/:id" => "problem#show"
-
+  root "plumbers#show"
+  get "plumbers" => "plumbers#index"
   resources :problems
 end
